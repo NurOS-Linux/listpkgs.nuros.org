1aecdf5e0dc6bd7539842920363ea8f2e34b5700
Мануэль Кегеян <kg.manyel@gmail.com>
2026-02-23 14:27:36 +0300

    fix: configure vite base path for gh-pages subdirectory deployment

--- listpkgs.nuros.front-end/vite.config.ts
+++ listpkgs.nuros.front-end/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
+  base: '/listpkgs.nuros.org/',
   plugins: [solid()],
   css: {
     preprocessorOptions: {
